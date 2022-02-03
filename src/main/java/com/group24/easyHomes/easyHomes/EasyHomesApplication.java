94a6674020fb543e545cdbc9992c052d3b54bd2e
vansia <dh608661@dal.ca>
2022-02-03 14:07:16 -0400

    Initial commit

---
+++ src/main/java/com/group24/easyHomes/easyHomes/EasyHomesApplication.java
@@ -0,0 +1,13 @@
+package com.group24.easyHomes.easyHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group24/easyHomes/easyHomes/EasyHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group24.easyHomes.easyHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
