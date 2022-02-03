112b82c85e7fe93bbd03201a0bb4256e84b93427
vansia <dh608661@dal.ca>
2022-02-03 14:30:57 -0400

    Refactor the code and make sure that project is running successfully.

--- src/main/java/com/group24/easyHomes/EasyHomesApplication.java
+++ src/main/java/com/group24/easyHomes/EasyHomesApplication.java
@@ -1,9 +1,10 @@
-package com.group24.easyHomes.easyHomes;
+package com.group24.easyHomes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EasyHomesApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/group24/easyHomes/EasyHomesApplicationTests.java
+++ src/test/java/com/group24/easyHomes/EasyHomesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.group24.easyHomes.easyHomes;
+package com.group24.easyHomes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
