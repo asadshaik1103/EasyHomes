24594b8023f60adcd0511af262dec0fbac1107d1
Shaik Asaduddin <shaikasaduddin@Shaiks-MacBook-Pro.local>
2022-03-01 21:11:41 -0400

    Revert "Home page feature"
    This reverts commit 790a2e7b19c9cdc53127734d94b3733fcee73dc2.

--- src/main/java/com/group24/easyHomes/repository/PropertyImagesRepository.java
+++ src/main/java/com/group24/easyHomes/repository/PropertyImagesRepository.java
@@ -3,4 +3,6 @@ package com.group24.easyHomes.repository;
 import com.group24.easyHomes.model.PropertyImages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PropertyImagesRepository extends JpaRepository<PropertyImages,String> { }
+public interface PropertyImagesRepository extends JpaRepository<PropertyImages,String> {
+
+}
--- src/main/java/com/group24/easyHomes/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.group24.easyHomes.repository;
-
-import com.group24.easyHomes.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service,Long> {}
