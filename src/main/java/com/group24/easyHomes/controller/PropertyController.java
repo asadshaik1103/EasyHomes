cc070c8d961aa82e45d4d8d3ae095430fff6b240
keyur <ky360972@dal.ca>
2022-03-09 21:37:22 -0400

    Solved back end CORS error

--- src/main/java/com/group24/easyHomes/controller/PropertyController.java
+++ src/main/java/com/group24/easyHomes/controller/PropertyController.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
+@RequestMapping(value = "/property ",produces = "application/json")
 public class PropertyController {
 
     @Autowired
