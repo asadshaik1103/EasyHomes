415dedcb58424f3aecf6609dc7f80d89eda5de63
keyur vaghani <ky360972@dal.ca>
2022-03-28 08:12:34 -0300

    feat(test): added service controller test case

--- src/test/java/com/group24/easyHomes/controller/ServiceControllerTest.java
+++ src/test/java/com/group24/easyHomes/controller/ServiceControllerTest.java
@@ -1,15 +1,43 @@
 package com.group24.easyHomes.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group24.easyHomes.model.Property;
+import com.group24.easyHomes.model.PropertyAddress;
+import com.group24.easyHomes.model.Services;
+import com.group24.easyHomes.service.PropertyService;
 import com.group24.easyHomes.service.ServicesService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,11 +51,27 @@ public class ServiceControllerTest {
     @Autowired
     private ObjectMapper mapper;
 
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
     @MockBean
     private ServicesService service;
 
     @Test
-   public void getServices() throws Exception {
+    @WithMockUser(username = "dv", password = "pwd", authorities = "USER")
+    public void getServices() throws Exception {
 
+        Services property1 = new Services();
+
+        List<Services> allServices = new ArrayList<>();
+        allServices.add(property1);
+
+        given(service.listAll()).willReturn(allServices);
+
+        mockMvc.perform(get("/service/services"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 }
