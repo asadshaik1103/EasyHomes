ca372c73a5935c712bd5944cb880233e6a028f55
keyur vaghani <ky360972@dal.ca>
2022-04-02 22:47:05 -0300

    feat(changed): changed service,property controler

--- src/main/java/com/group24/easyHomes/resource/impl/AppUserResourceImpl.java
+++ src/main/java/com/group24/easyHomes/resource/impl/AppUserResourceImpl.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @AllArgsConstructor
 public class AppUserResourceImpl {
 
